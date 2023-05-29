6c2152d130d1016bc154c642b0e6d62eb2889285
Girish Bennur <115867099+Girishpb@users.noreply.github.com>
2023-05-29 19:39:04 +0530

    Update nft_payment.sol

--- nft_payment.sol
+++ nft_payment.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT_USE_PAYMENTS is ERC721Enumerable, Ownable {
+  using Strings for uint256;
+
+  string public baseURI;
+  string public baseExtension = ".json";
+  uint256 public cost = 0.05 ether;
+  uint256 public maxSupply = 10000;
+  uint256 public maxMintAmount = 20;
+  bool public paused = false;
+  mapping(address => bool) public whitelisted;
+  address payable public payments;
+
+  constructor(
